ce85ca2d35691fcaffe0222ad6c36522c4c1896f
Nils <niskredd@nmbu.no>
2020-06-03 11:13:40 +0200

    added functins

--- biosim/aminal.py
+++ biosim/aminal.py
@@ -1,12 +1,24 @@
+
 animal_type = enumerate(['herbavor', 'carnevor'], 1)
+
 
 class animal:
 
     age = 0
     weight = 0
-    pos = (0,0)
+    pos = (0, 0)
     typ = animal_type
     fitness = 0
 
     def __init__(self):
         pass
+
+    def age_update(self):
+        pass
+
+    def weight_update(self):
+        pass
+
+    def clac_fitness(self):
+        pass
+
