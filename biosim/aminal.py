f98206be6c2351b182e188effb208fc29dfbc7b7
Nils <niskredd@nmbu.no>
2020-06-03 11:09:33 +0200

    added file animal.py
    some content

---
+++ biosim/aminal.py
@@ -0,0 +1,13 @@
+animal_type = enumerate(['herbavor', 'carnevor'], 1)
+
+class animal:
+
+    age = 0
+    weight = 0
+    pos = (0,0)
+    typ = animal_type
+    fitness = 0
+
+    def __init__(self):
+        pass
+    
