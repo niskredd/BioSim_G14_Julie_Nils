7deb3ca930034d5621b3e51675aee5466c4b57cb
Nils <niskredd@nmbu.no>
2020-06-03 14:25:39 +0200

    moved params to simulation
    added to  construkturs

--- biosim/aminal.py
+++ biosim/aminal.py
@@ -1,18 +1,3 @@
-params = {'w_birth': 8.,
-          'sigma_birth': 1.5,
-          'beta': 0.9,
-          'eta': 0.05,
-          'a_half': 40.,
-          'phi_age': 0.2,
-          'w_half': 10.,
-          'phi_weight': 0.1,
-          'mu': 0.25,
-          'gamma': 0.2,
-          'zeta': 3.5,
-          'xi': 1.2,
-          'omega': 0.4,
-          'F': 10.}
-
 
 class Animal:
 
@@ -44,11 +29,11 @@ class Herbivore(Animal):
         self.params = param
 
     def eat(self, food):
-        gain = params.beta * params.F * food
+        gain = self.params('beta') * self.params('F') * food
         self._weight_update(gain)
 
 
 class Carnivore(Animal):
 
-    def __init__(self):
-        super.__init__()
+    def __init__(self, age, pos, weight):
+        super.__init__(age, pos, weight)
--- biosim/simulation.py
+++ biosim/simulation.py
@@ -7,6 +7,36 @@
 __author__ = 'Julie Martin, Nils Skreddernes'
 __email__ = ''
 
+herb_params = {'w_birth': 8.,
+               'sigma_birth': 1.5,
+               'beta': 0.9,
+               'eta': 0.05,
+               'a_half': 40.,
+               'phi_age': 0.2,
+               'w_half': 10.,
+               'phi_weight': 0.1,
+               'mu': 0.25,
+               'gamma': 0.2,
+               'zeta': 3.5,
+               'xi': 1.2,
+               'omega': 0.4,
+               'F': 10.}
+
+carn_params = {'w_birth': 6.,
+               'sigma_birth': 1.,
+               'beta': 0.75,
+               'eta': 0.125,
+               'a_half': 40.,
+               'phi_age': 0.3,
+               'w_half': 4.,
+               'phi_weight': 0.4,
+               'mu': 0.4,
+               'gamma': 0.8,
+               'zeta': 3.5,
+               'xi': 1.5,
+               'omega': 0.8,
+               'F': 50.,
+               'DeltaPhiMax': 10.}
 
 from .aminal import *
 
