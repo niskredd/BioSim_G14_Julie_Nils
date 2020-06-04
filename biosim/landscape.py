792e95eebea464ae36d0d49eaef1b8893be541df
Nils <niskredd@nmbu.no>
2020-06-04 13:27:36 +0200

    Moved
    birth and death to tile

--- biosim/landscape.py
+++ biosim/landscape.py
@@ -1,3 +1,4 @@
+from .animal import *
 
 class Island:
 
@@ -30,3 +31,48 @@ class Tile:
 
     def fauna(self, species, age, weight):
         self.herb.append(Herbivore)
+
+    def birth(self, animal):
+        if animal.w < animal.params['zeta'](animal.params['w_birth'] + animal.params['sigma_birth']):
+            return None
+        else:
+            prob = min(1, animal.params['gamma'] * animal.phi * (animal.herb.__len__() - 1))
+            if random.rand() < prob:
+                new_born = Herbivore(0, 0)
+                animal.w = new_born.w * animal.params['zeta']
+
+                return new_born
+            else:
+                return None
+
+    def death(self):
+        if self.w == 0:
+            return True
+        else:
+            probability = self.params['omega'] * (1 - self.phi)
+            if random.rand() < probability:
+                return False
+            else:
+                return True
+
+if __name__ == '__main__':
+    mini_map = Tile((1, 1))
+
+    mini_map.fauna('Herbivore', 10, 12.5)
+    mini_map.fauna('Herbivore', 9, 10.5)
+
+    while 1:
+        """
+        eat
+        update animal info
+        birth
+        """
+        new = mini_map.birth(mini_map.herb[0])
+        if new is not None:
+            mini_map.herb.append(new)
+
+        """
+        death
+        
+        update fodder
+        """
