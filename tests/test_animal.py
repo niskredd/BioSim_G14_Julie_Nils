bc2b0fa1fb3133972c531c24c62ea6880d4eb2ca
Nils <niskredd@nmbu.no>
2020-06-08 10:20:55 +0200

    adding to test_animal

--- tests/test_animal.py
+++ tests/test_animal.py
@@ -2,36 +2,45 @@ import pytest
 from biosim.animal import Animal, Herbivore, Carnivore
 
 
-class Animal_testing:
+class Test_Animal:
 
     @pytest.fixture()
-    def create_carn(self):
-        carn = Carnivore(5, 10)
-        return carn
+    def create_ani(self):
+        anim = Animal(5, 10)
+        return anim
+
+    def test_age_update(self):
+        for i in range(3):
+            self.create_ani.age_update()
+        assert 8 == self.create_ani.a
+
+    def test_wight_update(self):
+        self.create_ani.weight_update(5)
+        assert self.create_ani.w == 5
+
+    def test_fitness(self):
+        self.create_ani.fitness_update()
+        assert 1 >= self.create_ani.phi >= 0
+
+    def test__new_born(self):
+        an = Animal(0, 0)
+        assert an.w != 0
+
+
+class Test_Herbivore:
 
     @pytest.fixture()
     def create_herb(self):
         herb = Herbivore(5, 10)
         return herb
 
-    def test_age(self):
-        for i in range(3):
-            self.create_herb._age_update()
-        assert 8 == self.create_herb.a
 
-    def test_wight(self):
-        an = Animal(2, 0)
-        an._weight_update(5)
-        assert an.w == 5
+class Test_Carnivore:
 
-    def test_fitness(self):
-        an = Animal(10, 10)
-        an.fitness_update()
-        assert 1 >= an.phi >= 0
-
-    def test__new_born(self):
-        an = Animal(0, 0)
-        assert an.w != 0
+    @pytest.fixture()
+    def create_carn(self):
+        carn = Carnivore(5, 10)
+        return carn
 
     def test_carnivores(self):
         assert self.create_carn.parms['ganna'] == 0.8
