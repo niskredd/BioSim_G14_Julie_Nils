3a8649b3c1f4c36207e883adddff48a6ee6d0a3f
Nils <niskredd@nmbu.no>
2020-06-10 12:42:45 +0200

    visualizing first test

--- biosim/visual.py
+++ biosim/visual.py
@@ -8,16 +8,29 @@ __author__ = 'Julie Martin, Nils Skreddernes'
 __email__ = ''
 
 
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib import style
+
+
 class Visual_Plot:
 
     def __init__(self):
-        pass
+        style.use('fivethirtyeight')
 
-    def setup_plot(self):
-        pass
-
-    def update_plot(self):
-        pass
+        fig = plt.figure()
+        self.ax1 = fig.add_subplot(1, 1, 1)
+        open('plotdata.csv', 'w')
 
     def animate(self):
-        pass
+        graph_data = open('plotdata.csv', 'a').read()
+        lines = graph_data.split('\n')
+        xs = []
+        ys = []
+        for line in lines:
+            if len(line) > 1:
+                x, y = line.split(',')
+                xs.append(float(x))
+                ys.append(float(y))
+        self.ax1.clear()
+        self.ax1.plot(xs, ys)
