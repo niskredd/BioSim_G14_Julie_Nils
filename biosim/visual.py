7461fa6d3ff18a626607ba14a1ff01c184da9225
Nils <niskredd@nmbu.no>
2020-06-16 11:09:18 +0200

    imports and restart

--- biosim/visual.py
+++ biosim/visual.py
@@ -7,30 +7,13 @@
 __author__ = 'Julie Martin, Nils Skreddernes'
 __email__ = ''
 
-
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-from matplotlib import style
+import numpy as np
+import subprocess
+import os
+from biosim.simulation import BioSim
 
 
 class Visual_Plot:
 
     def __init__(self):
-        style.use('fivethirtyeight')
-
-        fig = plt.figure()
-        self.ax1 = fig.add_subplot(1, 1, 1)
-        open('plotdata.csv', 'w')
-
-    def animate(self):
-        graph_data = open('plotdata.csv', 'a').read()
-        lines = graph_data.split('\n')
-        xs = []
-        ys = []
-        for line in lines:
-            if len(line) > 1:
-                x, y = line.split(',')
-                xs.append(float(x))
-                ys.append(float(y))
-        self.ax1.clear()
-        self.ax1.plot(xs, ys)
