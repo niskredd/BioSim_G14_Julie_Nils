bb6f27b656e3c13e34e2d8f27716f718803a997b
Julie Martin <julie.martin@nmbu.no>
2020-06-17 23:49:43 +0200

    Testing Island class

--- tests/test_island.py
+++ tests/test_island.py
@@ -1,12 +1,19 @@
 
 import pytest
-from biosim.landscape import Island
+from biosim.island import Island, Water
 
-class Test_Island:
+
+class TestIsland:
 
     @pytest.fixture()
     def create_island(self):
         return Island("WWW\nWDW\nWWW")
 
-    def test_create_island(self, create_island):
-        create_island.create_island()
+    def test_tiles_list(self, create_island):
+        island = create_island
+        assert island.tiles_lists.__len__() == 3
+        assert isinstance(island.tiles_lists[0][0], Water)
+
+    def test_map_test(self, create_island):
+        create_island.map_test("")
+
